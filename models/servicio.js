65d26ff678fa087bcf30f7d4f378f78e77156bd0
mudvaineorgy <mudvaine_orgy@hotmail.com>
2018-03-20 08:55:03 -0600

    Ultima actualizacion antes de las adecuaciones

--- models/servicio.js
+++ models/servicio.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 var servicioSchema = new Schema({
     nombre: { type: String, required: [true, 'El	nombre	es	necesario'] },
     img: { type: String, required: [false, 'null'] },
